2991c8493bcb20eac13aba47463adeecd87d7fce
StarkAwersay <66225429+StarkAwersay@users.noreply.github.com>
2022-02-19 20:54:21 +0400

    testMail

---
+++ untitled/src/test/java/contants/contants.java
@@ -0,0 +1,10 @@
+package contants;
+
+public class contants {
+    public static final String MAILPAIG = "https://mail.yandex.ru/";
+    public static final String LOGIN = "seeshstark";
+    public static final String PASSWORD = "Qwertyreverse1337";
+    public static final String MYMAIL = "Seeshstark@yandex.ru";
+    public static final String SUBJECT = "Simbirsoft theme";
+
+}
