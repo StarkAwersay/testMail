39a2ffdd79c63a8e83f67dc87afc18f259d2a642
StarkAwersay <66225429+StarkAwersay@users.noreply.github.com>
2022-02-28 18:05:38 +0400

    Переименовал GetEmail

--- src/test/java/responseApi/ResponseUserApi.java
+++ src/test/java/responseApi/ResponseUserApi.java
@@ -7,7 +7,7 @@ import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.given;
 
-public class GetEmail {
+public class ResponseUserApi {
     public static RequestSpecification returnRequestSpecification() {
         RequestSpecification requestSpecification = RestAssured.given();
         requestSpecification.baseUri("https://reqres.in/api");
--- src/test/java/tests/TestApi.java
+++ src/test/java/tests/TestApi.java
@@ -4,8 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static constants.Constants.*;
-import static responseApi.GetEmail.getEmail;
-import static responseApi.GetEmail.getEmailPagination;
+import static responseApi.ResponseUserApi.getEmail;
+import static responseApi.ResponseUserApi.getEmailPagination;
 
 public class TestApi {
     @Test
